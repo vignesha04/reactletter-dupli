b2de5f1f81aaaf7c8c7d5d1c193fb74407782ad8
Vignesh Aalathur <85474399+vignesha04@users.noreply.github.com>
2024-06-03 15:16:49 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -32,6 +32,7 @@ const LoveLetter = () => {
   return (
     <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
       <h1 className="grey-text text-lighten-4"> Rotate Your screen 🔄 and Tap on below Image, use 🎧 </h1>
+      <button onClick={toggleVisibility}>Toggle Header</button>
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
