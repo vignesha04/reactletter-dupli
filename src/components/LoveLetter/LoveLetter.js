befc0427c2d9767b2f231b390bda2cdb5c30d54b
Vignesh Aalathur <85474399+vignesha04@users.noreply.github.com>
2024-06-03 14:31:31 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -44,6 +44,31 @@ const LoveLetter = () => {
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
   );
+return (
+      <footer className="page-footer">
+                <div className="container">
+                  <div className="row">
+                    <div className="col l6 s12">
+                      <p className="grey-text text-lighten-4"> ReactJs Application deployment by using Kubernetes.</p>
+                      <p className="grey-text text-lighten-4"> Workflow of the Application deployment:</p>
+                      <p className="grey-text text-lighten-4"> Git → Jenkins → SonarQube → Docker → Kubernetes</p>
+                    </div>
+                    <div className="col l4 offset-l2 s12">
+                      <h5 className="white-text">Links</h5>
+                      <ul>
+                        <li><a className="grey-text text-lighten-3" href="#!">click here</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="footer-copyright">
+                  <div className="container">
+                  © 2024 Copyright Text
+                  <a className="grey-text text-lighten-4 right" href="#!">Thank You</a>
+                  </div>
+                </div>
+              </footer>
+    );
 };
 
 export default LoveLetter;
