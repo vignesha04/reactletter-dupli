65eaa018dd69e920d1bcfafe2b2bae08a90357aa
Vignesh Aalathur <85474399+vignesha04@users.noreply.github.com>
2024-06-05 16:21:39 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -38,7 +38,7 @@ const LoveLetter = () => {
         Dear MICKY❤️👑,<br />
         The day is UR's, Just Be Happy, Healthy and Love yourself😊. I'm Always happy to see you doing well and i pray for the Best of You🥰..<br /> 
         Really Missed youuu 🥹 ❤️HAPPY BIRTHDAY MICKY👑🎂🫰🏻<br /> 
-        Eid Milad Saeed meri jaan❤️👑, Taqabal Allah Oyatak🤲🏻 👸<br /> Take Care Always
+        Eid Milad Saeed meri jaan❤️👑, Taqabal Allah Oyatak🤲🏻 👸<br /> Always take care Raa.
         -Your's ❤️🙂  
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
